4ef54ac5100a6a5bb10a9205f61a484c6f5b69a3
Edwin Tellman <etellman@pobox.com>
2023-07-11 07:33:58 -0700

    Simplify ListTest
    Use --> instead of eq.

--- test/Ch07/ListTest.hs
+++ test/Ch07/ListTest.hs
@@ -6,8 +6,8 @@ import qualified Hedgehog.Range as Range
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
-eq :: ([Int] -> [Int]) -> (Int -> Int) -> PropertyT IO ()
-eq f' f = do
+(-->) :: (Int -> Int) -> ([Int] -> [Int]) -> PropertyT IO ()
+(-->) f f' = do
   xs <- forAll $ Gen.list (Range.constant 0 10) (Gen.int $ Range.constant 0 100)
 
   cover 2 "empty" $ null xs
@@ -15,20 +15,16 @@ eq f' f = do
 
   f' xs === map f xs
 
-prop_identity :: Property
-prop_identity =
-  property $ do
-    (fmap id) `eq` id
+infixr 0 -->
 
 prop_morphism :: Property
 prop_morphism =
   property $ do
     -- set up
     n <- forAll $ Gen.int (Range.constant 2 100)
-    let f = (+ n)
 
     -- exercise and verify
-    fmap f `eq` f
+    (+ n) --> fmap (+ n)
 
 prop_compose :: Property
 prop_compose =
@@ -41,13 +37,13 @@ prop_compose =
         g = (* n)
 
     -- exercise and verify
-    (fmap f . fmap g) `eq` (f . g)
+    f . g --> fmap f . fmap g
 
 tests :: TestTree
 tests =
   testGroup
     "List"
-    [ testProperty "identity" prop_identity,
+    [ testProperty "identity" $ property $ id --> fmap id,
       testProperty "morphism" prop_morphism,
       testProperty "compose" prop_compose
     ]
