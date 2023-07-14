632ccf67b58b4a1c48e02d5a5fc2369bf265aa26
Edwin Tellman <etellman@pobox.com>
2023-07-14 07:16:45 -0700

    Refactor K2 and PreList tests
    Use the common bifunctor test logic.

--- test/Ch08/K2Test.hs
+++ test/Ch08/K2Test.hs
@@ -1,12 +1,11 @@
 module Ch08.K2Test (tests) where
 
+import Ch08.BifunctorTest
 import Ch08.K2
-import Data.Bifunctor
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 import Test.Tasty
-import Test.Tasty.Hedgehog
 
 (-->) ::
   ((Int -> Int), (Int -> Int)) ->
@@ -17,41 +16,5 @@ import Test.Tasty.Hedgehog
 
   fg (K2 x) === K2 x
 
-infixr 0 -->
-
-prop_morphism :: Property
-prop_morphism =
-  property $ do
-    -- set up
-    m <- forAll $ Gen.int (Range.constant (-100) 100)
-    n <- forAll $ Gen.int (Range.constant (-100) 100)
-
-    let f = (+ m)
-        g = (* n)
-
-    -- exercise and verify
-    (f, g) --> bimap f g
-
-prop_compose :: Property
-prop_compose =
-  property $ do
-    -- set up
-    m <- forAll $ Gen.int (Range.constant (-100) 100)
-    n <- forAll $ Gen.int (Range.constant (-100) 100)
-
-    let f = (+ m)
-        g = (* m)
-        h = (* n)
-        j = (+ n)
-
-    -- exercise and verify
-    (f . h, g . j) --> (bimap f g) . (bimap h j)
-
 tests :: TestTree
-tests =
-  testGroup
-    "K2"
-    [ testProperty "identity" $ property $ (id, id) --> bimap id id,
-      testProperty "morphism" prop_morphism,
-      testProperty "compose" prop_compose
-    ]
+tests = bifunctorTests "K2" (-->)
--- test/Ch08/PreListTest.hs
+++ test/Ch08/PreListTest.hs
@@ -1,12 +1,11 @@
 module Ch08.PreListTest (tests) where
 
+import Ch08.BifunctorTest
 import Ch08.PreList
-import Data.Bifunctor
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 import Test.Tasty
-import Test.Tasty.Hedgehog
 
 (-->) ::
   ((Int -> Int), (Int -> Int)) ->
@@ -19,41 +18,5 @@ import Test.Tasty.Hedgehog
   fg (Cons x y) === Cons (f x) (g y)
   fg Nil === Nil
 
-infixr 0 -->
-
-prop_morphism :: Property
-prop_morphism =
-  property $ do
-    -- set up
-    m <- forAll $ Gen.int (Range.constant (-100) 100)
-    n <- forAll $ Gen.int (Range.constant (-100) 100)
-
-    let f = (+ m)
-        g = (* n)
-
-    -- exercise and verify
-    (f, g) --> bimap f g
-
-prop_compose :: Property
-prop_compose =
-  property $ do
-    -- set up
-    m <- forAll $ Gen.int (Range.constant (-100) 100)
-    n <- forAll $ Gen.int (Range.constant (-100) 100)
-
-    let f = (+ m)
-        g = (* m)
-        h = (* n)
-        j = (+ n)
-
-    -- exercise and verify
-    (f . h, g . j) --> (bimap f g) . (bimap h j)
-
 tests :: TestTree
-tests =
-  testGroup
-    "PreList"
-    [ testProperty "identity" $ property $ (id, id) --> bimap id id,
-      testProperty "morphism" prop_morphism,
-      testProperty "compose" prop_compose
-    ]
+tests = bifunctorTests "PreList" (-->)
