2f3d2b750fb188e785b40b89ed24952b8ee2c061
Edwin Tellman <etellman@pobox.com>
2023-07-17 05:54:01 -0700

    Add CartesianTest to Ch09/ChapterTest

--- test/Ch09/ChapterTest.hs
+++ test/Ch09/ChapterTest.hs
@@ -1,11 +1,13 @@
 module Ch09.ChapterTest (chapterTests) where
 
 import Ch09.EvalTest
+import Ch09.CartesianTest
 import Test.Tasty
 
 chapterTests :: TestTree
 chapterTests =
   testGroup
     "Chapter 9"
-    [ Ch09.EvalTest.tests
+    [ Ch09.EvalTest.tests,
+      Ch09.CartesianTest.tests
     ]
