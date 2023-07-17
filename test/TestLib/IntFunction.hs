2d53eb97fb651019218bc29949f8733ec3d55245
Edwin Tellman <etellman@pobox.com>
2023-07-17 07:34:45 -0700

    Give int functions meaningful names

--- test/TestLib/IntFunction.hs
+++ test/TestLib/IntFunction.hs
@@ -3,22 +3,24 @@ module TestLib.IntFunction (intFunction) where
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
+import Text.Printf
 
 data NamedFunction a b = NamedFunction String (a -> b)
+
+operator :: String -> Int -> (Int -> Int -> Int) -> NamedFunction Int Int
+operator name n f = NamedFunction (printf "(%s %d)" name n) (f n)
 
 instance Show (NamedFunction a b) where
   show (NamedFunction name _) = name
 
-genIntFunction :: String -> Gen (NamedFunction Int Int)
-genIntFunction name = do
+genIntFunction :: Gen (NamedFunction Int Int)
+genIntFunction = do
   n <- Gen.int (Range.constant 2 1000)
-  let fs = fmap (NamedFunction name) [(+ n), ((-) n), (* n)]
+  let fs = [ (operator "+" n (+)), (operator "-" n (-)), (operator "*" n (*)) ]
 
   Gen.element fs
 
 intFunction :: PropertyT IO (Int -> Int)
 intFunction = do
-  name <- forAll $ Gen.string (Range.constant 5 5) Gen.alpha
-  (NamedFunction _ f) <- forAll $ genIntFunction name
-
+  (NamedFunction _ f) <- forAll $ genIntFunction
   return f
