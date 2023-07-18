f56131c58108d7833e297397b9258da6fbe72d15
Edwin Tellman <etellman@pobox.com>
2023-07-18 07:17:02 -0700

    Remove duplicate Const length test

--- test/Ch10/ChapterTest.hs
+++ test/Ch10/ChapterTest.hs
@@ -1,6 +1,5 @@
 module Ch10.ChapterTest (chapterTests) where
 
-import Ch10.ConstTest
 import Ch10.FGNaturalTest
 import Ch10.ListNaturalTest
 import Test.Tasty
@@ -10,6 +9,5 @@ chapterTests =
   testGroup
     "Chapter 10"
     [ Ch10.FGNaturalTest.tests,
-      Ch10.ListNaturalTest.tests,
-      Ch10.ConstTest.tests
+      Ch10.ListNaturalTest.tests
     ]
