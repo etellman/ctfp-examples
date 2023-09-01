a983816f903b66f0292e3912fd7621ff0bb59813
Edwin Tellman <etellman@pobox.com>
2023-09-01 08:40:17 -0700

    Add alternate reader monad instance

---
+++ src/Ch21/Reader2.hs
@@ -0,0 +1,35 @@
+module Ch21.Reader2
+  ( Reader2 (..),
+    runReader2,
+  )
+where
+
+import Control.Applicative
+
+newtype Reader2 e a = Reader2 (e -> a)
+
+runReader2 :: Reader2 e a -> e -> a
+runReader2 (Reader2 f) e = f e
+
+instance Functor (Reader2 e) where
+  fmap f (Reader2 g) = Reader2 (f . g)
+
+instance Applicative (Reader2 e) where
+  pure x = Reader2 (const x)
+  liftA2 ::
+    (f -> g -> h) ->
+    Reader2 e f ->
+    Reader2 e g ->
+    Reader2 e h
+  liftA2 k (Reader2 x) (Reader2 y) =
+    let z e = k (x e) (y e)
+     in Reader2 z
+
+instance Monad (Reader2 e) where
+  (>>=) :: Reader2 e a -> (a -> Reader2 e b) -> Reader2 e b
+  readerA >>= aToReaderB =
+    let f e =
+          let a = runReader2 readerA e
+              readerB = aToReaderB a
+           in runReader2 readerB e
+     in Reader2 f
