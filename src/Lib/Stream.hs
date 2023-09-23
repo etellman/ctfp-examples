e074f2a579e2721068c09165c31befc623ae3d73
Edwin Tellman <etellman@pobox.com>
2023-09-23 08:00:02 -0700

    Add Comonad instance for Stream

--- src/Lib/Stream.hs
+++ src/Lib/Stream.hs
@@ -4,13 +4,11 @@ module Lib.Stream
   )
 where
 
+import Control.Comonad
 import Data.Distributive
 import Data.Functor.Rep
 
 data Stream a = Cons a (Stream a) deriving (Show)
-
-streamHead :: Stream a -> a
-streamHead (Cons x _) = x
 
 streamTail :: Stream a -> Stream a
 streamTail (Cons _ xs) = xs
@@ -30,7 +28,7 @@ instance (Eq a) => Eq (Stream a) where
 
 instance Distributive Stream where
   distribute :: Functor f => f (Stream a) -> Stream (f a)
-  distribute xs = Cons (fmap streamHead xs) (distribute $ fmap streamTail xs)
+  distribute xs = Cons (fmap extract xs) (distribute $ fmap streamTail xs)
 
   collect :: Functor f => (a -> Stream b) -> f a -> Stream (f b)
   collect h = distribute . fmap h
@@ -45,3 +43,10 @@ instance Representable Stream where
   index (Cons b bs) n
     | n == 0 = b
     | otherwise = index bs (n - 1)
+
+instance Comonad Stream where
+  extend :: (Stream a -> b) -> Stream a -> Stream b
+  extend f as@(Cons _ as') = Cons (f as) (extend f as')
+
+  extract :: Stream a -> a
+  extract (Cons a _) = a
--- test/Lib/StreamTest.hs
+++ test/Lib/StreamTest.hs
@@ -1,6 +1,7 @@
 module Lib.StreamTest (tests) where
 
 import Ch10.NaturalProperty
+import Control.Comonad
 import Data.Distributive as Dist
 import Data.Functor.Rep
 import Hedgehog as H
@@ -12,15 +13,25 @@ import Test.Tasty.Hedgehog
 import TestLib.Assertions
 import TestLib.IntFunction
 
--- | verifies that f(x) == g(x) for natural numbers
+eqF ::
+  (Int -> a) ->
+  (a -> (Int -> Int)) ->
+  (a -> (Int -> Int)) ->
+  PropertyT IO ()
+eqF toM f g = do
+  x <- forAll $ Gen.int (Range.constant (-100) 100)
+  n <- forAll $ Gen.int (Range.constant 0 100)
+
+  (f . toM) x n === (g . toM) x n
+
+-- | verifies that f(x) == g(x) for non-negative numbers
 eqNonNegative :: (Show a, Eq a) => (Int -> a) -> (Int -> a) -> PropertyT IO ()
 f `eqNonNegative` g = do
   x <- forAll $ Gen.int (Range.constant 0 100)
   f x === g x
 
 prop_memoizedMatchesOriginal :: Property
-prop_memoizedMatchesOriginal =
-  property $ do
+prop_memoizedMatchesOriginal = property $ do
     f <- intFunction
     let xs = tabulate f :: Stream Int
 
@@ -43,21 +54,33 @@ prop_collect =
     -- exercise and verify
     Dist.collect countingStream (Just x) === fmap Just (countingStream x)
 
-eqF ::
-  (Int -> a) ->
-  (a -> (Int -> Int)) ->
-  (a -> (Int -> Int)) ->
-  PropertyT IO ()
-eqF toM f g = do
+prop_extend :: Property
+prop_extend = property $ do
+  -- set up
   x <- forAll $ Gen.int (Range.constant (-100) 100)
-  n <- forAll $ Gen.int (Range.constant 0 100)
+  y <- forAll $ Gen.int (Range.constant (-100) 100)
+  f <- intFunction
 
-  (f . toM) x n === (g . toM) x n
+  let xs = countingStream x
+  let withY = Cons y xs
+
+  -- exercise and verify
+  extend (fmap f) withY === Cons (fmap f withY) (extend (fmap f) xs)
+
+prop_extract :: Property
+prop_extract = property $ do
+  -- set up
+  x <- forAll $ Gen.int (Range.constant (-100) 100)
+  y <- forAll $ Gen.int (Range.constant (-100) 100)
+  let xs = Cons x (countingStream y)
+
+  -- exercise and verify
+  extract xs === x
 
 tests :: TestTree
 tests =
   testGroup
-    "Ch14.StreamTest"
+    "Lib.StreamTest"
     [ testGroup
         "Memoized Function"
         [ testProperty "memoized value matches" prop_memoizedMatchesOriginal,
@@ -70,5 +93,10 @@ tests =
         "Distributive"
         [ testProperty "distribute Maybe" prop_distribute,
           testProperty "collect Maybe" prop_collect
+        ],
+      testGroup
+        "Comonad"
+        [ testProperty "extend" prop_extend,
+          testProperty "extract" prop_extract
         ]
     ]
