cfb09ccb4f29412b421723cc144ec3300b6a77a7
Edwin Tellman <etellman@pobox.com>
2023-09-29 06:14:53 -0700

    More catamorphism

--- test/Ch23/CatamorphismTest.hs
+++ test/Ch23/CatamorphismTest.hs
@@ -8,19 +8,28 @@ import qualified Hedgehog.Range as Range
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
+-- add intermediate steps to clarify what's going on
+m :: Fix NatF -> Int
+m x =
+  let nextLevel = unfix x :: NatF (Fix NatF)
+      natural = fmap m nextLevel :: NatF Int
+      alg = natToInt
+   in alg natural
+
 prop_commute :: Property
 prop_commute = property $ do
   -- set up
   n <- forAll $ Gen.int (Range.constant 0 100)
   let ffx = fmap Fix $ intToNat n :: NatF (Fix NatF)
-      m = natToInt . fmap m . unfix :: Fix NatF -> Int
+      m' = natToInt . fmap m' . unfix
 
   -- exercise and verify
   (m . Fix) ffx === (natToInt . fmap m) ffx
+  (m' . Fix) ffx === (natToInt . fmap m') ffx
 
 tests :: TestTree
 tests =
   testGroup
     "Ch23.CatamorphismTest"
-    [ testProperty "non-zero" $ prop_commute
+    [ testProperty "commute" $ prop_commute
     ]
